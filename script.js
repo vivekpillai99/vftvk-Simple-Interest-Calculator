c7f968682f52da50c507a8fab8810f2836540788
Vivek Pillai <vpillai72@gmail.com>
2021-10-23 03:15:14 +0000

    added JS functionality

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute(){
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear+parseInt(years);
+    document.getElementById("result").innerText="If you deposit "+principal+",\<br\> at at interest rate of"+
+    interest+"%.\<br\> You will receive an amount of "+interest+",\<br\> in the year "+ year + "\<br\>" 
 }
-        
+
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("r4ate_val").innerText=rateval;
+}
